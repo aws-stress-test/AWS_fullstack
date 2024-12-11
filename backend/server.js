d3a7e385ef28c6406d11294a010d9d21330fb836
sofia <me@Sofia-MacBook-Pro.local>
2024-12-11 16:37:29 +0900

    feat: cors 추가

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,8 @@ const corsOptions = {
     'http://localhost:3000',
     'https://localhost:3000',
     'http://0.0.0.0:3000',
-    'https://0.0.0.0:3000'
+    'https://0.0.0.0:3000',
+    'https://goorm-ktb-018.goorm.team'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
