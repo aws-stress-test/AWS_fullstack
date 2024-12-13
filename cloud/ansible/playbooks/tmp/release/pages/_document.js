317958ed3703a62313de4028ece2953b0831e347
sofia <me@Sofia-MacBook-Pro.local>
2024-12-13 10:07:10 +0900

    test

--- cloud/ansible/playbooks/tmp/release/pages/_document.js
+++ cloud/ansible/playbooks/tmp/release/pages/_document.js
@@ -1,28 +1,19 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="ko">
-        <Head>
-          <meta charSet="utf-8" />
-          <link
-            rel="preload"
-            href="https://statics.goorm.io/gds/foundation/v0.19.0/vapor-foundation.dark.min.css"
-            as="style"
-          />
-          <link
-            rel="stylesheet"
-            href="https://statics.goorm.io/gds/foundation/v0.19.0/vapor-foundation.dark.min.css"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html lang="ko">
+      <Head>
+        <meta charSet="utf-8" />
+        <link
+          rel="stylesheet"
+          href="https://statics.goorm.io/gds/foundation/v0.19.0/vapor-foundation.dark.min.css"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default MyDocument;
